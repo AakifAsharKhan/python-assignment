4991ff11c349c14818a3ab66453d8df4fdb01cba
Aakif Ashar Khan <65507937+AakifAsharKhan@users.noreply.github.com>
2020-11-06 00:47:21 +0530

    Add files via upload
    Made an assignment on the collatz sequence

---
+++ collatz_assignment.py
@@ -0,0 +1,18 @@
+def collatz(number):
+    if (number % 2 == 0):
+        number = number // 2
+        print(number)
+        return int(number)
+    elif (number % 2 == 1):
+        number = 3 * number + 1
+        print(number)
+        return int(number)
+    else:
+        return number
+
+number = int(input("Enter the integer"))
+some_number = number
+while(True):
+    some_number = collatz(some_number)
+    if some_number == 1:
+        break
